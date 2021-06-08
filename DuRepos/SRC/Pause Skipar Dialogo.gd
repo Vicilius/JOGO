8aa16aac6da095df5ca1de4b63f9c1dfb67e19b4
Vicilius <viniciusmachado19@gmail.com>
2021-06-08 00:05:43 -0300

    aaaa

---
+++ DuRepos/SRC/Pause Skipar Dialogo.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+
+
+
+
+func _on_Button_pressed():
+	
+	$Sound2.play()
+	pass # Replace with function body.
+
+
+func _on_Button2_pressed():
+	$Sound.play()
+	pass # Replace with function body.
