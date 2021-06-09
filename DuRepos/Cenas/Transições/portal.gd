dc59663598d696ed29f67f991194bf6e7c384053
Vicilius <viniciusmachado19@gmail.com>
2021-06-09 14:35:14 -0300

    aaa

---
+++ DuRepos/Cenas/Transições/portal.gd
@@ -0,0 +1,19 @@
+tool
+
+extends Area2D
+
+export(String, FILE) var next_scene_path = ""
+
+
+func _get_configuration_warning() -> String:
+		if next_scene_path == "":
+			return "next_scene_path must be set to the portal to work"
+		else:
+			return ""
+
+
+func _on_portal_body_entered(body):
+	if get_tree().change_scene(next_scene_path) != OK:
+		
+		print("Error: Unavailable scene!")
+	pass # Replace with function body.
